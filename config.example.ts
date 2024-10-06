8c9e6ae5dd10f44175aac96aab238aadc5852bb3
Baran Mordoğan <baran@okunamayanad.com>
2024-10-06 14:32:08 +0300

    Refactor gitignore and config files

---
+++ config.example.ts
@@ -0,0 +1,21 @@
+// RENAME THIS FILE TO config.ts
+
+export default {
+  whoami: "node1", // name of the node
+  interval: 1000, // interval in ms
+  timeout: 5000, // timeout in ms
+  offlineThreshold: 5, // threshold for a node to be considered offline (per interval)
+  nodes: [
+    // list of nodes
+    {
+      name: "node1",
+      ip: "1.1.1.1",
+      port: 37214,
+    },
+    {
+      name: "node2",
+      ip: "2.2.2.2",
+      port: 37214,
+    },
+  ],
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { isIPInRangeOrPrivate } from "range_check";
 import config from "./config.ts";
 
 interface NodeStatus {
@@ -19,35 +20,41 @@ setInterval(async () => {
 
     console.log(`Pinging ${node.name} on port ${node.port}`);
 
-    try {
-      const controller = new AbortController();
-      const timeoutId = setTimeout(() => {
-        console.error(`Request to ${node.name} timed out`);
-        controller.abort();
-      }, config.timeout);
+    const controller = new AbortController();
+    const nodeTimeoutTimer = setTimeout(() => {
+      // Timeout logic
+      console.error(`Request to ${node.name} timed out`);
+      controller.abort();
 
-      try {
-        await fetch(`http://${node.ip}:${node.port}`, {
-          signal: controller.signal,
-        });
-        console.log(`Successfully pinged ${node.name}`);
-        nodeStatus[node.name].timeouts = 0; // Reset timeout counter on success
-        nodeStatus[node.name].online = true; // Mark node as online on success
-      } finally {
-        clearTimeout(timeoutId);
-      }
-    } catch (e) {
-      console.error(`Failed to ping ${node.name}`);
       nodeStatus[node.name].timeouts += 1;
 
       if (nodeStatus[node.name].timeouts >= 5) {
         nodeStatus[node.name].online = false;
         console.error(`Node ${node.name} marked as offline`);
-      }
-    }
 
-    if (!nodeStatus[node.name].online) {
-      console.log(`Node ${node.name} is currently offline`);
+        console.log("Nodes status:");
+        console.table(nodeStatus);
+      }
+    }, config.timeout);
+
+    try {
+      await fetch(`http://${node.ip}:${node.port}`, {
+        signal: controller.signal,
+      });
+      console.log(`Successfully pinged ${node.name}`);
+      if (!nodeStatus[node.name].online) {
+        console.log(`Node ${node.name} marked as online`);
+      }
+      nodeStatus[node.name].timeouts = 0;
+      nodeStatus[node.name].online = true;
+    } catch (err: any) {
+      if (err.name === "AbortError") {
+        continue;
+      }
+
+      console.error(`Error pinging ${node.name}: ${err}`);
+    } finally {
+      clearTimeout(nodeTimeoutTimer);
     }
   }
 }, config.interval);
@@ -56,6 +63,10 @@ Bun.serve({
   port: config.nodes.find((node) => node.name === config.whoami)!.port,
   fetch(req, server) {
     const ip = server.requestIP(req)?.address;
+
+    if (!ip) throw new Error("IP address cannot be determined");
+
+    if (isIPInRangeOrPrivate(ip)) return new Response("hello world");
 
     if (!config.nodes.find((node) => node.ip === ip))
       return new Response("Unauthorized", { status: 401 });
